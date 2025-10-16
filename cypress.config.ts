63bc664ab39cb85f20e8712fceadde8adb39a47e
JanHakanViktor <Janhakanv@gmail.com>
2025-10-16 17:15:18 +0200

    Setup first tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "cypress";
+import { seedRecipes } from "./prisma/seed/recipe";
+import { db } from "./prisma/db";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on) {
+      on("task", {
+        async reseed() {
+          await db.recipe.deleteMany();
+          await seedRecipes();
+          return null;
+        },
+      });
     },
   },
 });
